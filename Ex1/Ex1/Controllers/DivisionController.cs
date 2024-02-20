92b5dc5a0d7493ed578cd5defe549c7a4bbf60cf
hachan140 <havu14102@gmail.com>
2024-02-21 00:59:46 +0700

    commit ex1

---
+++ Ex1/Ex1/Controllers/DivisionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ex1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DivisibleController : ControllerBase
+    {
+        [HttpGet("{number}")]
+        public IActionResult IsDivisibleBy2024(int number)
+        {
+            if (number % 2024 == 0)
+            {
+                return Ok(new { DivisibleBy2024 = true });
+            }
+            else
+            {
+                return Ok(new { DivisibleBy2024 = false });
+            }
+        }
+    }
+}
---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://127.0.0.1:5500")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors("AllowSpecificOrigin");
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
